975c7290c7ca999c40e12138acbba2866cdd2ea1
Michael <kilcorse.michael92@gmail.com>
2023-05-24 20:56:33 -0400

    Clean up Menu component

--- portal/src/components/ui/Menu.tsx
+++ portal/src/components/ui/Menu.tsx
@@ -1,54 +1,59 @@
-import { Dispatch, ReactNode, SetStateAction, useState, useRef, useEffect } from "react";
-import { FaPlay }from 'react-icons/fa'
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+  useRef,
+  useEffect,
+} from 'react';
+import { FaPlay } from 'react-icons/fa';
 import WindowWrapper from './WindowWrapper';
 
 type OptionsArr = {
-    isOpen: Dispatch<SetStateAction<boolean>>;
-    options: string[]
-}
+  isOpen: Dispatch<SetStateAction<boolean>>;
+  options: string[];
+};
 
-export default function Menu( {options, isOpen}: OptionsArr){
-    const select = useRef<any>(null);
-    const [cursorLoc, setCursor] = useState(0);
-    // const [ selected, setSelected ] = useState(false)
+export default function Menu({ options, isOpen }: OptionsArr) {
+  const select = useRef<any>(null);
+  const [cursorLoc, setCursor] = useState(0);
+  //for future navigation
+  // const [ selected, setSelected ] = useState(false)
 
-    function arrowHandler(event: any){
-        if(event.key === "ArrowUp" && cursorLoc > 0){
-            setCursor(cursorLoc => cursorLoc - 1)
-            console.log(cursorLoc);
-        }
-        if(event.key === "ArrowDown" && cursorLoc < (options.length - 1)){
-            setCursor(cursorLoc => cursorLoc + 1)
-            console.log(cursorLoc)
-        }
-        console.log(event.key)
-        
+  function arrowHandler(event: any) {
+    //clean up conditional later
+    if (event.key === 'ArrowUp' && cursorLoc > 0) {
+      setCursor((cursorLoc) => cursorLoc - 1);
+      console.log(cursorLoc);
     }
-
-    // Is it better to use seperate useEffects here?
-    useEffect(() => {
-        window.addEventListener("keydown", arrowHandler);
-        return () => {
-            window.removeEventListener("keydown", arrowHandler);
-          };
-
-    });
-    return(
-        <>
-        <WindowWrapper isOpen={isOpen}>
-            <ul>
-                {options.map((option: string, i): ReactNode => {
-                    return  (
-                        <li ref={select} id={"select" + i.toString()} key={option + i}>
-                            <div className="flex p-1 m-1">
-                                {cursorLoc === i ? <FaPlay /> : null}
-                                {option}
-                            </div>
-                        </li>
-                        )})
-                }
-            </ul>
-        </WindowWrapper>
-        </>
-    );
+    if (event.key === 'ArrowDown' && cursorLoc < options.length - 1) {
+      setCursor((cursorLoc) => cursorLoc + 1);
+      console.log(cursorLoc);
+    }
+    console.log(event.key);
+  }
+  useEffect(() => {
+    window.addEventListener('keydown', arrowHandler);
+    return () => {
+      window.removeEventListener('keydown', arrowHandler);
+    };
+  });
+  return (
+    <>
+      <WindowWrapper isOpen={isOpen}>
+        <ul>
+          {options.map((option: string, i): ReactNode => {
+            return (
+              <li ref={select} id={'select' + i.toString()} key={option + i}>
+                <div className='flex p-1 m-1'>
+                  {cursorLoc === i ? <FaPlay /> : null}
+                  {option}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </WindowWrapper>
+    </>
+  );
 }
