4df6ae6f5529876027ad94c7d1405e8c77b190b3
Michael <kilcorse.michael92@gmail.com>
2023-05-31 07:30:34 -0400

    WIP: Convert console to screen border and begin to add responsive styling

--- portal/src/components/game_console/GameConsole.tsx
+++ portal/src/components/game_console/GameConsole.tsx
@@ -3,20 +3,14 @@ import GameScreen from './GameScreen';
 export default function GameConsole() {
   return (
     <>
-      <div className='flex justify-center min-h-screen bg-gray-50 py-6 sm:py-12'>
-        <div className='relative h-[500px] content-center w-[300px] rounded-t-md rounded-b-lg bg-purple-500'>
-          <div className='absolute inset-x-5 rounded top-4 h-[240px] w-[260px] bg-slate-500 z-10'>
-            <div
-              id='screen'
-              className='hide-scrollbar absolute overflow-auto inset-x-10 top-6 h-[180px] w-[180px] z-20 bg-slate-300'
-            >
-              <GameScreen />
-            </div>
-            <div className='absolute left-3 top-12 h-[10px] w-[10px] z-20 bg-red-500 rounded-full'></div>
-            <div className='text-white font-sans text-[0.5rem] absolute top-14 p-1'>
-              POWER
-            </div>
-            <div className='text-slate-100 font-sans right-20 absolute bottom-1'>
+      <div className='flex h-screen justify-center items-center px-10 lg:px-12 w-screen bg-purple-500'>
+        <div className='flex container rounded justify-center items-center h-[90%] w-screen  bg-slate-500 z-10'>
+          <div
+            id='screen'
+            className='hide-scrollbar justify-center items-center overflow-auto h-[90%] w-[90%] bg-slate-300'
+          >
+            <GameScreen />
+            <div className='absolute right-0 z-30 text-slate-100 font-sans'>
               POKERBOI <span className='text-red-500'>C</span>
               <span className='text-purple-300'>o</span>
               <span className='text-green-300'>l</span>
@@ -24,31 +18,10 @@ export default function GameConsole() {
               <span className='text-blue-300'>r</span>
             </div>
           </div>
-          <div className='bg-slate-500 absolute left-12 bottom-40 h-[35px] w-[25px]'>
-            <div className='border-solid top-2 left-[.15rem] border-slate-600 border-b-[15px] border-x-transparent border-x-[10px] border-t-0 absolute'></div>
+          <div className='absolute left-3 top-12 h-[15px] w-[15px] md:h-[20px] md:w-[20px] lg:h-[30px] lg:w-[30px] z-20 bg-red-500 rounded-full'></div>
+          <div className='text-white font-sans text-[0.75rem] lg:text-lg absolute top-14 p-1'>
+            POWER
           </div>
-          <div className='bg-slate-500 absolute left-[4.5rem] bottom-[8.5rem] h-[25px] w-[35px]'>
-            <div className='absolute border-solid border-l-slate-600 top-[0.15rem] left-3  border-l-[15px] border-y-transparent border-y-[10px] border-r-0'></div>
-          </div>
-          <div className='bg-slate-500 absolute left-12 bottom-[6.5rem] h-[35px] w-[25px]'>
-            <div className='absolute top-2 left-[.15rem] border-solid border-t-slate-600 border-t-[15px] border-x-transparent border-x-[10px] border-b-0'></div>
-          </div>
-          <div className='bg-slate-500 absolute left-[0.80rem] bottom-[8.5rem] h-[25px] w-[35px]'>
-            <div className='absolute border-solid border-r-slate-600 border-r-[15px] top-[0.15rem] left-2 border-y-transparent border-y-[10px] border-l-0'></div>
-          </div>
-          <div className='bg-slate-500 absolute left-11 bottom-[8.5rem] h-[25px] w-[28px]'></div>
-          <div className='absolute right-24 bottom-24 rounded-full bg-slate-600 h-10 w-10 z-20'>
-            <h1 className='text-slate-300 font-sans absolute left-[.80rem] top-[.15rem] text-2xl'>
-              B
-            </h1>
-          </div>
-          <div className='absolute right-10 bottom-32 rounded-full bg-slate-600 h-10 w-10 z-20'>
-            <h1 className='text-slate-300 font-sans absolute left-[.80rem] top-[.15rem] text-2xl'>
-              A
-            </h1>
-          </div>
-          <div className='absolute h-2 w-6 bottom-16 left-28 bg-slate-600 rounded-full'></div>
-          <div className='absolute h-2 w-6 bottom-16 right-28 bg-slate-600 rounded-full'></div>
         </div>
       </div>
     </>
