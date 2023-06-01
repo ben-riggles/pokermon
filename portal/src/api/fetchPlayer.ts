0905a1796b26fed6bfb879c7a413dd5c75165f2d
Jack Trusler <jtrus93@gmail.com>
2023-06-01 16:05:31 -0400

    deleted fetch player

--- portal/src/api/fetchPlayer.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export async function fetchPlayer() {
-  const response = await fetch('http://localhost:5000/players');
-  console.log(response);
-  const res = await axios.get('http://localhost:5000/players');
-  console.log(res);
-}
