f7dc2c4702d5361ea91c4096bcc82a6d0f2971d5
Michael <kilcorse.michael92@gmail.com>
2023-05-31 21:19:46 -0400

    Update stylings to finished border

--- portal/src/components/game_console/GameConsole.tsx
+++ portal/src/components/game_console/GameConsole.tsx
@@ -3,24 +3,30 @@ import GameScreen from './GameScreen';
 export default function GameConsole() {
   return (
     <>
-      <div className='flex h-screen justify-center items-center px-10 lg:px-12 w-screen bg-purple-500'>
-        <div className='flex container rounded justify-center items-center h-[90%] w-screen  bg-slate-500 z-10'>
+      <div className='flex h-screen justify-center items-center w-screen bg-purple-800'>
+        <div
+          id='power-display'
+          className='absolute min-[280px]:invisible   md:visible min-[900px]: top-1/4 left-1 ml-1'
+        >
+          <div className='mx-auto h-[30px] w-[30px] relative shadow-[0_0_40px_10px_#FF0000]  z-30  bg-red-700 rounded-full' />
+          <div className='text-white font-sans :text-lg'>POWER</div>
+        </div>
+        <div
+          id='outer-screen'
+          className='flex flex-col rounded justify-center items-center h-[90%] w-screen mx-5 md:mx-20 bg-slate-500 z-10'
+        >
           <div
             id='screen'
             className='hide-scrollbar justify-center items-center overflow-auto h-[90%] w-[90%] bg-slate-300'
           >
             <GameScreen />
-            <div className='absolute right-0 z-30 text-slate-100 font-sans'>
-              POKERBOI <span className='text-red-500'>C</span>
-              <span className='text-purple-300'>o</span>
-              <span className='text-green-300'>l</span>
-              <span className='text-yellow-100'>o</span>
-              <span className='text-blue-300'>r</span>
-            </div>
           </div>
-          <div className='absolute left-3 top-12 h-[15px] w-[15px] md:h-[20px] md:w-[20px] lg:h-[30px] lg:w-[30px] z-20 bg-red-500 rounded-full'></div>
-          <div className='text-white font-sans text-[0.75rem] lg:text-lg absolute top-14 p-1'>
-            POWER
+          <div className='z-30 text-lg mt-2 text-slate-100 font-sans'>
+            POKERBOI <span className='text-red-500'>C</span>
+            <span className='text-purple-300'>o</span>
+            <span className='text-green-300'>l</span>
+            <span className='text-yellow-100'>o</span>
+            <span className='text-blue-300'>r</span>
           </div>
         </div>
       </div>
