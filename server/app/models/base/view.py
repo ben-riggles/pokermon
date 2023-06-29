c314847e56240053909b3e910c301aeae933c471
philophoseal <benjamin.riggs.38@gmail.com>
2023-06-28 21:35:35 -0400

    remove print

--- server/app/models/base/view.py
+++ server/app/models/base/view.py
@@ -56,7 +56,6 @@ class ViewModel(ABC):
 @dataclass
 class DirectView(ViewModel):
     def __init__(self, model):
-        print('hello')
         for field in class_fields(self):
             try:
                 val = getattr(model, field.name)
