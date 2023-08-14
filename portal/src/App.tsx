e9a61efe78796aeadf4a553096f4587987cb9e79
jacktrusler <jtrus93@gmail.com>
2023-08-14 17:52:19 -0400

    added react query devtools

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import LandingPage from '@/components/pages/LandingPage';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <LandingPage />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
